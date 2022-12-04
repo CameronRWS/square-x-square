df1be52ba0f35021c20b0287e0ac9d7013f34ed3
Cameron <CameronRWS@gmail.com>
2022-12-04 15:21:43 -0600

    idk

---
+++ Classes/GameState.cs
@@ -0,0 +1,95 @@
+﻿
+
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+
+namespace Classes;
+
+public class GameState
+{
+
+    private int[,] Grid { get; set; }
+
+    private List<Block> BlocksPlaced { get; set; }
+
+    private int _rows = 6;
+    private int _cols = 6;
+
+    public GameState()
+    {
+        Grid = new int[_rows, _cols];
+        BlocksPlaced = new List<Block>();
+    }
+
+    public bool CheckLocationAndAddBlock(Block b)
+    {
+        if(IsBlockLocationValid(b))
+        {
+            AddBlock(b);
+            return true; 
+        } 
+        return false;
+    }
+
+    private void AddBlock(Block b)
+    {
+        foreach (var position in b.TilePositions())
+        {
+            Grid[position.Row, position.Column] = b.Id;
+        }
+        BlocksPlaced.Add(b);
+    }
+
+    public void RemoveBlockAtPosition(Position p)
+    {
+        foreach(var b in BlocksPlaced)
+        {
+            foreach(var pos in b.TilePositions())
+            {
+                if(p.Row == pos.Row && p.Column == pos.Column)
+                {
+                    RemoveBlock(b);
+                    return;
+                }
+            }
+        }
+    }
+
+    private void RemoveBlock(Block b)
+    {
+        foreach(var pos in b.TilePositions())
+        {
+            Grid[pos.Row, pos.Column] = 0;
+        }
+    }
+
+    public bool IsBlockLocationValid(Block b)
+    {
+        foreach(var position in b.TilePositions())
+        {
+            if (Grid[position.Row, position.Column] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    override public string ToString()
+    {
+        String s = string.Empty;
+        for(int r = 0; r < _rows; r++)
+        {
+            s += "[";
+            for (int c = 0; c < _cols; c++)
+            {
+                s = s + Grid[r, c] + ", ";
+            }
+            s = s.Substring(0, s.Length - 2);
+            s += "]," + "\n";
+        }
+        return s;
+    }
+}
---
+++ Classes/IBlock.cs
@@ -0,0 +1,28 @@
+﻿namespace Classes
+{
+    public class IBlock : Block
+    {
+        private readonly Position[][] tiles = new Position[][]
+        {
+            //non-flipped
+            new Position[] { new(1, 0), new (1, 1), new(1, 2), new(1,3) }, //flat bar on top half
+            new Position[] { new(0, 2), new (1, 2), new(2, 2), new(3,2) }, //tall bar on right half
+            new Position[] { new(2, 0), new (2, 1), new(2, 2), new(2,3) }, //flat bar on bottom half
+            new Position[] { new(0, 1), new (1, 1), new(2, 1), new(3,1) }, //tall bar on left half
+            //flipped
+            new Position[] { new(2, 0), new (2, 1), new(2, 2), new(2,3) }, //flat bar on bottom half
+            new Position[] { new(0, 1), new (1, 1), new(2, 1), new(3,1) }, //tall bar on left half
+            new Position[] { new(1, 0), new (1, 1), new(1, 2), new(1,3) }, //flat bar on top half
+            new Position[] { new(0, 2), new (1, 2), new(2, 2), new(3,2) }  //tall bar on right half
+        };
+
+        public IBlock(int id)
+        {
+            Id = id;
+        }
+
+        protected override Position StartOffset => new Position(0, 0);
+
+        protected override Position[][] Tiles => tiles;
+    }
+}
